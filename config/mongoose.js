7c433baa7d855fcec500a3729fabc085ad883561
Cheviti Soma Sai Dinesh <80689758+somasaic@users.noreply.github.com>
2024-04-14 19:56:36 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1/placement_cell');
+//mongoose.connect('mongodb://127.0.0.1/placement_cell');
+mongoose.connect("mongodb+srv://mallireddy1999916:HfDKCaxbDaJZhvxm@cluster0.9tn9rqp.mongodb.net/placement_cell");
 
 const db = mongoose.connection;
 
